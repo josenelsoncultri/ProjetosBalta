f202b6a15b55ad415f1a87e4702fd6240f15e7a2
José Nelson Cultri <josefcultrifrc@gmail.com>
2025-03-25 21:37:43 -0300

    Criando classes do contexto de conteúdo

---
+++ 2802-OOP/Balta/ContentContext/Content.cs
@@ -0,0 +1,8 @@
+namespace Balta.ContentContext;
+
+public class Content
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+}
--- 2802-OOP/Balta/obj/Debug/net9.0/Balta.AssemblyInfo.cs
+++ 2802-OOP/Balta/obj/Debug/net9.0/Balta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Balta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5833be89a1ffb69cce6d78cfccf821c2bfa6405")]
 [assembly: System.Reflection.AssemblyProductAttribute("Balta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Balta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
