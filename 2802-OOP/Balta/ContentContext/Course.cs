dbf0698204ade3d1ec2162d4ab38cd8fb7a672ae
José Nelson Cultri <josefcultrifrc@gmail.com>
2025-03-25 21:46:04 -0300

    Adicionando classes

--- 2802-OOP/Balta/ContentContext/Course.cs
+++ 2802-OOP/Balta/ContentContext/Course.cs
@@ -2,5 +2,31 @@ namespace Balta.ContentContext;
 
 public class Course : Content
 {
+    public Course()
+    {
+        Modules = new List<Module>();
+    }
 
+    public string Tag { get; set; } = string.Empty;
+
+    public IList<Module> Modules { get; set; } = null!;
+}
+
+public class Module
+{
+    public Module()
+    {
+        Lectures = new List<Lecture>();
+    }
+
+    public int Order { get; set; }
+    public string Title { get; set; } = string.Empty;
+
+    public IList<Lecture> Lectures { get; set; }
+}
+
+public class Lecture
+{
+    public int Order { get; set; }
+    public string Title { get; set; } = string.Empty;
 }
