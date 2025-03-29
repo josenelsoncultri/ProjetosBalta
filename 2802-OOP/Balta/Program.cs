3fdb4917873beedb92b2b49715bef5fa53764e22
José Nelson Cultri <josefcultrifrc@gmail.com>
2025-03-29 08:18:29 -0300

    Ordenação de Listas

--- 2802-OOP/Balta/Program.cs
+++ 2802-OOP/Balta/Program.cs
@@ -12,3 +12,32 @@ foreach (var article in articles)
     Console.WriteLine(article.Title);
     Console.WriteLine(article.Url);
 }
+
+var courses = new List<Course>();
+var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+var courseCsharp = new Course("Fundamentos C#", "fundamentos-csharp");
+var courseAspnet = new Course("Fundamentos ASP.NET", "fundamentos-aspnet");
+courses.Add(courseOOP);
+courses.Add(courseCsharp);
+courses.Add(courseAspnet);
+
+var careers = new List<Career>();
+var careerDotnet = new Career("Especialista .NET", "especialista-dotnet");
+var careerItem3 = new CareerItem(2, "OOP", "", null);
+var careerItem2 = new CareerItem(3, "Aprenda .NET", "", null);
+var careerItem = new CareerItem(1, "Comece por aqui", "", null);
+careerDotnet.Items.Add(careerItem2);
+careerDotnet.Items.Add(careerItem3);
+careerDotnet.Items.Add(careerItem);
+careers.Add(careerDotnet);
+
+foreach (var career in careers)
+{
+    Console.WriteLine(career.Title);
+
+    foreach (var item in career.Items.OrderBy(x => x.Order))
+    {
+        Console.WriteLine($"{item.Order} - {item.Title}");
+    }
+}
+
