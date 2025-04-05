a419ebb4f8908cbe823003ba59b73655b43137ca
José Nelson Cultri <josefcultrifrc@gmail.com>
2025-04-05 09:40:55 -0300

    Visualizando progresso do aluno

---
+++ 2805-FundamentosSQLServer/007-VisualizingStudentsProgress.sql
@@ -0,0 +1,13 @@
+DECLARE @StudentId UNIQUEIDENTIFIER = '014B38ED-C48C-4A3C-B9CF-EC9F00973017'
+
+SELECT
+	Student.Name AS Student,
+	Course.Title AS Course,
+	StudentCourse.Progress
+FROM StudentCourse
+INNER JOIN Student ON (Student.Id = StudentCourse.StudentId)
+INNER JOIN Course ON (Course.Id = StudentCourse.CourseId)
+WHERE 1 = 1
+AND Student.Id = @StudentId
+AND StudentCourse.Progress < 100
+AND StudentCourse.Progress > 0
