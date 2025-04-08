16f9b3d7534b13ee75961cef81b4b9bc972b7e0f
José Nelson Cultri <josefcultrifrc@gmail.com>
2025-04-08 20:59:59 -0300

    Iniciando criação do projeto

---
+++ 2806-AcessoADadosDapperSQLServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Models;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+const string CONNECTION_STRING = "Server=NITRO-JN\\MSSQLSERVER2022;Database=Blog;User Id=balta;Password=balta;Trust Server Certificate=True";
+
+ReadUsers();
+
+static void ReadUsers()
+{
+    using var connection = new SqlConnection(CONNECTION_STRING);
+    var users = connection.GetAll<User>();
+
+    foreach (var user in users)
+    {
+        Console.WriteLine(user.Name);
+    }
+}
