741c34d3c32f19a678a516908ba8c383b358365e
José Nelson Cultri <josefcultrifrc@gmail.com>
2025-04-10 19:52:27 -0300

    Criando models

---
+++ 2806-AcessoADadosDapperSQLServer/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace Blog.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Summary { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public DateTime CreateDate { get; set; }
+    public DateTime LastUpdateDate { get; set; }
+
+    public int AuthorId { get; set; }
+    [Write(false)]
+    public User Author { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+    [Write(false)]
+    public Category Category { get; set; } = null!;
+
+    [Write(false)]
+    public List<Tag> Tags { get; set; } = [];
+}
