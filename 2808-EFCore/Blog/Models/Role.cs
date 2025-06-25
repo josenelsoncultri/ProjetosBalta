0ab2d1afa519738fb5a4ba2d5be1b6fb8a181fab
José Nelson Cultri <josefcultrifrc@gmail.com>
2025-06-24 22:15:25 -0300

    Criando modelos para curso de EFCore

---
+++ 2808-EFCore/Blog/Models/Role.cs
@@ -0,0 +1,8 @@
+namespace Blog.Models;
+
+public class Role
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+}
