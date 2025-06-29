add0a4c2351813239017550954242a00bcebe362
José Nelson Cultri <josefcultrifrc@gmail.com>
2025-06-29 18:12:33 -0300

    Iniciando mapeamento da entidade de categorias com DataAnnotations

--- 2808-EFCore/Blog/Models/Category.cs
+++ 2808-EFCore/Blog/Models/Category.cs
@@ -1,8 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models;
 
+[Table("Category")]
 public class Category
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(80)]
+    [Column("Name", TypeName = "NVARCHAR")]
     public string Name { get; set; } = string.Empty;
+
+
+    [Required]
+    [MaxLength(80)]
+    [Column("Name", TypeName = "VARCHAR")]
     public string Slug { get; set; } = string.Empty;
 }
